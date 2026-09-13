cf84bae6887c5483bec6ec23ad3aef933d23f867
markgsimon <mark.simon0917@gmail.com>
2026-09-13 09:29:38 -0400

    feat(temp-calc): add temperature calculator

---
+++ celsius-fahr-calc/cels-fahr-calc.c
@@ -0,0 +1,38 @@
+#include <stdio.h> 
+
+
+/*
+* The purpose of this program is print a table of fahrenheit temperatures
+* and their celsius equivalents given the following formula:
+* C = (5/9) (F - 32)
+* for fahr = 0,.... 300
+*/
+
+int main() {
+
+
+   // Variable declaration
+   int fahr, celsius;
+   int lower, upper, step;
+
+
+   // Variable instantiaton
+   lower = 0;     // lower limit of temperature
+   upper = 300;   // upper limit of temperature
+   step = 20;     // step in between temperatures to calculate, e.g. +20 
+
+
+   // write code here to do the work
+   printf("\n%-10s | %s\n", "Fahrenheit", "Celsius");
+   printf("%-10s-+-%s\n", "----------", "-------");
+   
+   fahr = lower;
+   while(fahr <= upper) {
+      celsius = 5 * (fahr - 32) / 9;
+      printf("%-10d | \t%d\n", fahr, celsius);
+      fahr = fahr + step; 
+   }
+
+   return 0;
+
+}
