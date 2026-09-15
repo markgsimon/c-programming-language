432fdd3e1ffffdecfe451524aad3aad3322c1993
markgsimon <mark.simon0917@gmail.com>
2026-09-15 06:11:38 -0400

    feat(temp-calc): modify to print in reverse order

--- celsius-fahr-calc/temp_calc_for_loop.c
+++ celsius-fahr-calc/temp_calc_for_loop.c
@@ -12,7 +12,7 @@ int main() {
    printf("\n%-10s | %s", "Fahrenheit", "Celsius");
    printf("\n%-10s-+-%s\n", "----------", "-------");
 
-   for(fahr = 0; fahr <= 300; fahr = fahr + 20) {
+   for(fahr = 300; fahr >= 0; fahr = fahr - 20) {
         printf("%10d | %6.1f\n", fahr, (5.0/9.0) * (fahr-32));
    }    
 
